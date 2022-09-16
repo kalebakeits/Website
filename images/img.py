9af17425066ca4258698730eaf101e252be8bf77
kalebakeits <sgkkeits@liverpool.ac.uk>
2022-09-16 10:58:55 +0100

    Fixed url for social icon anchor tags

--- images/img.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import numpy as np
-import random
-data = [[random.randint(0,1) for __ in range(500)] for _ in range(100)]
-plt.imsave('filename.png', np.array(data).reshape(500,100), cmap=cm.gray)
